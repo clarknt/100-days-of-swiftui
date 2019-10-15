596d57a8de30e04b058bd2d9809dbcf3f2d2f5f6
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-15 19:47:02 -0400

    Update bundle ID

---
+++ Project3/Project3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project3
+//
+//  Created by clarknt on 2019-10-15.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
