beeff0745d16343a3ae8acd501bf85202bebd4de
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-24 20:16:18 -0400

    Project 6 - Part 1 - Implicit animations

---
+++ 10-Project6/Project6/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Project6
+//
+//  Created by clarknt on 2019-10-24.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+
+    var body: some View {
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .animation(.default)
+        .blur(radius: (animationAmount - 1) * 3)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
