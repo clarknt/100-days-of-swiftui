904784c5e6bc4b151d275354dd77f26388090fbf
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-27 20:52:00 -0400

    Project 6 - Access all animations

--- 10-Project6/Project6/ContentView.swift
+++ 10-Project6/Project6/ContentView.swift
@@ -8,9 +8,38 @@
 
 import SwiftUI
 
+struct NamedView {
+    var name: String
+    var view: AnyView
+
+    init<V>(_ name: String, view: V) where V: View {
+        self.name = name
+        self.view = AnyView(view)
+    }
+}
+
 struct ContentView: View {
+    private let animations: [NamedView] = [
+        NamedView("Implicit animation", view: ImplicitAnimation()),
+        NamedView("Custom animation", view: CustomAnimation()),
+        NamedView("Bindings animation", view: BindingsAnimation()),
+        NamedView("Explicit animation", view: ExplicitAnimation()),
+        NamedView("Animation stack", view: AnimationStack()),
+        NamedView("Animated gestures - 1", view: AnimatedGestures1()),
+        NamedView("Animated gestures - 2", view: AnimatedGestures2()),
+        NamedView("Show/hide view animation", view: ShowHideViewAnimation()),
+        NamedView("Custom transition", view: CustomTransition())
+    ]
+
     var body: some View {
-        CustomTransition()
+        NavigationView {
+            List(0..<animations.count) { i in
+                NavigationLink(destination: self.animations[i].view) {
+                    Text(self.animations[i].name)
+                }
+            }
+            .navigationBarTitle("Animations")
+        }
     }
 }
 
