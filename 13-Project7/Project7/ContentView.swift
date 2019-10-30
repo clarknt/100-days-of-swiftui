9b0471d9727022e3e7b798fa087484351ab21f99
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-29 21:54:11 -0400

    Project 7 - Part 2

--- 13-Project7/Project7/ContentView.swift
+++ 13-Project7/Project7/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        StateAndStruct()
+        ObservedObjects()
     }
 }
 
---
+++ 13-Project7/Project7/ObservedObjects.swift
@@ -0,0 +1,35 @@
+//
+//  ObservedObjects.swift
+//  Project7
+//
+//  Created by clarknt on 2019-10-29.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct ObservedObjects: View {
+    @ObservedObject var user = User()
+
+    var body: some View {
+        VStack(alignment: HorizontalAlignment.center, spacing: 10) {
+            Text("Class (changes are reflected thanks to @Published / @ObservedObject)")
+
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
+
+struct ObservedObjects_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservedObjects()
+    }
+}
