bd96d71674e924dcf25fb1f9f74ae1ab348fe469
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-29 22:06:22 -0400

    Project 7 - Part 3

---
+++ 13-Project7/Project7/ShowHideViews.swift
@@ -0,0 +1,45 @@
+//
+//  ShowHideViews.swift
+//  Project7
+//
+//  Created by clarknt on 2019-10-29.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    @Environment(\.presentationMode)var presentationMode
+
+    var name: String
+
+    var body: some View {
+        VStack {
+            Text("Hello, \(name)")
+
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+            .offset(x: 0, y: 20)
+        }
+    }
+}
+
+struct ShowHideViews: View {
+    @State private var showingSheet = false
+
+    var body: some View {
+        Button("Show Sheet") {
+            self.showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Second View")
+        }
+    }
+}
+
+struct ShowHideViews_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowHideViews()
+    }
+}
