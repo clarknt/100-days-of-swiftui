1c9b85998c0f1ec3d127652d0d59cd978ee9aad6
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-01 22:58:43 -0400

    Project 8 - Part 1

---
+++ 14-Project8/Project8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project8
+//
+//  Created by clarknt on 2019-11-01.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 14-Project8/Project8/Introduction/MainView.swift
@@ -0,0 +1,42 @@
+//
+//  MainView.swift
+//  Project8
+//
+//  Created by clarknt on 2019-11-01.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct NamedView {
+    var name: String
+    var view: AnyView
+
+    init<V>(_ name: String, view: V) where V: View {
+        self.name = name
+        self.view = AnyView(view)
+    }
+}
+
+struct MainView: View {
+    private let topics: [NamedView] = [
+        NamedView("Resizing images to fit the screen using GeometryReader", view: ResizeImage()),
+    ]
+
+    var body: some View {
+        NavigationView {
+            List(0..<topics.count) { i in
+                NavigationLink(destination: self.topics[i].view) {
+                    Text(self.topics[i].name)
+                }
+            }
+            .navigationBarTitle("Topics")
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
