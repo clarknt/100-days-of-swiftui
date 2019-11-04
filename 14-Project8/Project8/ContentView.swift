8e27c8ee3209fcea2b49967959ed1cf16f15c2c4
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-03 23:23:27 -0500

    Project 8 - Part 9

--- 14-Project8/Project8/ContentView.swift
+++ 14-Project8/Project8/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(missions) { mission in
-                NavigationLink(destination: Text("Detail View")) {
+                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
                     Image(mission.image)
                         .resizable()
                         .scaledToFit() // shortcut for .aspectRatio(contentMode: .fit)
--- 14-Project8/Project8/MissionView.swift
+++ 14-Project8/Project8/MissionView.swift
@@ -8,8 +8,31 @@
 
 import SwiftUI
 
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
 struct MissionView: View {
     let mission: Mission
+    let astronauts: [CrewMember]
+
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+
+        var matches = [CrewMember]()
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            }
+            else {
+                fatalError("Missing \(member)")
+            }
+        }
+
+        self.astronauts = matches
+    }
 
     var body: some View {
         GeometryReader { geometry in
@@ -24,6 +47,32 @@ struct MissionView: View {
                     Text(self.mission.description)
                     .padding()
 
+                    ForEach(self.astronauts, id: \.role) { crewMember in
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 166, height: 120)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.primary, lineWidth: 1))
+
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .font(.headline)
+                                HStack {
+                                    Text(crewMember.role)
+                                        .foregroundColor(.secondary)
+                                    if crewMember.role == "Commander" {
+                                        Image(systemName: "star.fill")
+                                            .foregroundColor(Color.yellow)
+                                    }
+                                }
+                            }
+
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+
                     Spacer(minLength: 25)
                 }
             }
@@ -34,8 +83,9 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
 
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
     }
 }
