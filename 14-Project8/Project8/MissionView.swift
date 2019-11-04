e873d20510084f725add32867ad4bcd2ee0c19cf
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-03 22:54:12 -0500

    Project 8 - Part 8

---
+++ 14-Project8/Project8/MissionView.swift
@@ -0,0 +1,41 @@
+//
+//  MissionView.swift
+//  Project8
+//
+//  Created by clarknt on 2019-11-03.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+
+                    Text(self.mission.description)
+                    .padding()
+
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
