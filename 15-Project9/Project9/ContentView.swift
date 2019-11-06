7c28ced5da7f6c661b34bf896f4510b040e84226
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-05 22:04:23 -0500

    Project 9 - Part 1

---
+++ 15-Project9/Project9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project9
+//
+//  Created by clarknt on 2019-11-05.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CustomPaths()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 15-Project9/Project9/Introduction/CustomPaths.swift
@@ -0,0 +1,28 @@
+//
+//  CustomPaths.swift
+//  Project9
+//
+//  Created by clarknt on 2019-11-05.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomPaths: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct CustomPaths_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomPaths()
+    }
+}
