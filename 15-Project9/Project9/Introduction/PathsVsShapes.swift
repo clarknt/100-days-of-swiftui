5df5fd65e4429ea58f22eaa90656c66c02017a5d
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-05 22:29:51 -0500

    Project 9 - Part 2

---
+++ 15-Project9/Project9/Introduction/PathsVsShapes.swift
@@ -0,0 +1,60 @@
+//
+//  PathsVsShapes.swift
+//  Project9
+//
+//  Created by clarknt on 2019-11-05.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        // compensate the fact that SwiftUI start the arc from the right instead of the top
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        // !clockwise because SwiftUI goes the other way around from one angle to the other
+
+        return path
+    }
+}
+
+struct PathsVsShapes: View {
+    var body: some View {
+        ZStack {
+            Triangle()
+                .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 300, height: 300)
+
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
+        }
+    }
+}
+
+struct PathsVsShapes_Previews: PreviewProvider {
+    static var previews: some View {
+        PathsVsShapes()
+    }
+}
