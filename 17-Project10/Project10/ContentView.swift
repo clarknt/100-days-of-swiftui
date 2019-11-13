2428aa92223e13ad87322bbf9d22000df1890d71
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-12 22:57:58 -0500

    Project 10 - Part 2

--- 17-Project10/Project10/ContentView.swift
+++ 17-Project10/Project10/ContentView.swift
@@ -8,27 +8,9 @@
 
 import SwiftUI
 
-enum CodingKeys: CodingKey {
-    case name
-}
-
-class User: ObservableObject, Codable {
-    @Published var name = "Paul Hudson"
-
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        CodableData()
     }
 }
 
