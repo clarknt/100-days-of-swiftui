974b8b7d08d61422f1587a7cf0c9f07cca0b1231
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-14 23:29:44 -0500

    Project 10 - Part 6

--- 17-Project10/Project10/CheckoutView.swift
+++ 17-Project10/Project10/CheckoutView.swift
@@ -12,7 +12,25 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
 
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
     }
 }
 
--- 17-Project10/Project10/Order.swift
+++ 17-Project10/Project10/Order.swift
@@ -37,5 +37,25 @@ class Order: ObservableObject {
 
         return true
     }
+
+    var cost: Double {
+        // $2 per cake
+        var cost = Double(quantity) * 2
+
+        // complicated cakes cost more
+        cost += (Double(type) / 2)
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+
+        return cost
+    }
 }
 
