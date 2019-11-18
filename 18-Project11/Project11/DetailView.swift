bf8671a8f8f67f23404c37ad93950f20ea18b9dc
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-18 16:54:33 -0500

    Project 11

---
+++ 18-Project11/Project11/DetailView.swift
@@ -0,0 +1,88 @@
+//
+//  DetailView.swift
+//  Project11
+//
+//  Created by clarknt on 2019-11-18.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import CoreData
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+
+    @State private var showingDeleteAlert = false
+
+    let book: Book
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+
+                Text(self.book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+
+                Text(self.book.review ?? "No review")
+                    .padding()
+
+                RatingView(rating: .constant(Int(self.book.rating)))
+                    .font(.largeTitle)
+
+                Spacer()
+            }
+        }
+        .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"),
+                  message: Text("Are you sure?"),
+                  primaryButton: .destructive(Text("Delete")) { self.deleteBook() },
+                  secondaryButton: .cancel()
+            )
+        }
+    }
+
+    func deleteBook() {
+        moc.delete(book)
+
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+
+        return NavigationView {
+            DetailView(book: book)
+        }
+        .colorScheme(.dark)
+    }
+}
---
+++ 18-Project11/Project11/RatingView.swift
@@ -0,0 +1,53 @@
+//
+//  RetingView.swift
+//  Project11
+//
+//  Created by clarknt on 2019-11-18.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+
+    var label = ""
+    var maximumRating = 5
+
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+
+    var offColor = Color.gray
+    var onColor = Color.yellow
+
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+
+            ForEach(1..<maximumRating + 1) { number in
+                self.image(for: number)
+                    .foregroundColor(number > self.rating ? self.offColor : self.onColor)
+                    .onTapGesture {
+                        self.rating = number
+                }
+            }
+        }
+    }
+
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        }
+        else {
+            return onImage
+        }
+    }
+}
+
+struct RetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
