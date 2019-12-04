0b6bacce1dff34c1035a30d65698fc50f9a3d870
clarknt <24315769+clarknt@users.noreply.github.com>
2019-12-04 11:08:55 -0500

    Project 13 - Part 6

--- 21-Project13/Project13/Introduction/UIViewControllerSwiftUI.swift
+++ 21-Project13/Project13/Introduction/UIViewControllerSwiftUI.swift
@@ -8,6 +8,16 @@
 
 import SwiftUI
 
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished")
+    }
+}
+
 struct UIViewControllerSwiftUI: View {
     @State private var image: Image?
     @State private var inputImage: UIImage?
@@ -31,6 +41,8 @@ struct UIViewControllerSwiftUI: View {
     func loadImage() {
         guard let inputImage = inputImage else { return }
         image = Image(uiImage: inputImage)
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
