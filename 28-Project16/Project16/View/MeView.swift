2c01966ee9c9245a9c1993049ef2ce35ce99578b
clarknt <24315769+clarknt@users.noreply.github.com>
2019-12-30 17:57:36 -0500

    Project 16 - Part 12

--- 28-Project16/Project16/View/MeView.swift
+++ 28-Project16/Project16/View/MeView.swift
@@ -6,11 +6,52 @@
 //  Copyright © 2019 clarknt. All rights reserved.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+
+                Spacer()
+            }
+            .navigationBarTitle("Your Code")
+        }
+    }
+
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
