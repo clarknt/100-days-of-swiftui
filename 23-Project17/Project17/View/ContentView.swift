a290866cd39a4d7f6bf21b8a40957e63118a28fd
clarknt <24315769+clarknt@users.noreply.github.com>
2020-01-07 10:21:46 -0500

    Project 17 - Part 8

--- 23-Project17/Project17/View/ContentView.swift
+++ 23-Project17/Project17/View/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+
     var body: some View {
-        CardView(card: Card.example)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index])
+                            .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
 }
 
