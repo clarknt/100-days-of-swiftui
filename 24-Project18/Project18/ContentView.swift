66765c40b0be4ca5c67d223c65cf79b223146aa2
clarknt <24315769+clarknt@users.noreply.github.com>
2020-01-10 15:56:14 -0500

    Project 18 - Part 1

---
+++ 24-Project18/Project18/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project18
+//
+//  Created by clarknt on 2020-01-09.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 24-Project18/Project18/Intro01Layout.swift
@@ -0,0 +1,54 @@
+//
+//  Intro01-Layout.swift
+//  Project18
+//
+//  Created by clarknt on 2020-01-10.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct Intro01Layout: View {
+    var body: some View {
+        TabView {
+            VStack {
+                Spacer()
+
+                Text("View->Background->Text")
+                    .background(Color.red)
+
+                Spacer()
+
+                Text("View->Background->Padding->Text")
+                    .padding(20)
+                    .background(Color.red)
+
+                Spacer()
+
+                Text("View->Padding->Background->Text")
+                    .background(Color.red)
+                    .padding()
+                    //.border(Color.gray)
+
+                Spacer()
+            }
+            .tabItem {
+                Image(systemName: "1.circle")
+                Text("Fixed size")
+            }
+
+
+            Color.red
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text("Neutral size")
+                }
+        }
+    }
+}
+
+struct Intro01Layout_Previews: PreviewProvider {
+    static var previews: some View {
+        Intro01Layout()
+    }
+}
