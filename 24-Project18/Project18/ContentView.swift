a90df89876bc2f51758123b21bb0602494e59d1e
clarknt <24315769+clarknt@users.noreply.github.com>
2020-01-13 23:01:02 -0500

    Project 18 - Part 6

--- 24-Project18/Project18/ContentView.swift
+++ 24-Project18/Project18/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Intro05FramesAndCoordinates()
+        Intro06ScrollViewEffect()
     }
 }
 
---
+++ 24-Project18/Project18/Intro06ScrollViewEffect.swift
@@ -0,0 +1,83 @@
+//
+//  Intro06ScrollView.swift
+//  Project18
+//
+//  Created by clarknt on 2020-01-13.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import SwiftUI
+
+struct Intro06ScrollViewEffect: View {
+    @State private var displayHelix = true
+
+    var body: some View {
+        Group {
+            if displayHelix {
+                Helix()
+            }
+            else {
+                CoverFlow()
+            }
+        }
+        .onTapGesture { self.displayHelix.toggle() }
+    }
+}
+
+struct Helix: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            //.rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY) / 5),
+                            //                  axis: (x: 0, y: 1, z: 0))
+                            .rotation3DEffect(
+                                .degrees(
+                                    Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5
+                                ),
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+struct CoverFlow: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, ((fullView.size.width - 150) / 2))
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Intro06ScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        Intro06ScrollViewEffect()
+    }
+}
