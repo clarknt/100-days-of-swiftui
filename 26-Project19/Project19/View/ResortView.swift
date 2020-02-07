715d189e541d6d679414c73c534abbdf631039b2
clarknt <24315769+clarknt@users.noreply.github.com>
2020-02-06 22:53:16 -0500

    Project 19 - Challenge 1

--- 26-Project19/Project19/View/ResortView.swift
+++ 26-Project19/Project19/View/ResortView.swift
@@ -39,6 +39,7 @@ struct ResortView: View {
                             }
                         }
                     )
+                    .clipped()
 
                 Group {
                     HStack {
