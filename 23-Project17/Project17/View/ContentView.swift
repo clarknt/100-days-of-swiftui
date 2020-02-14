b864478af593a7ccfe5f8d3fdbaf9ed12e52c94d
clarknt <24315769+clarknt@users.noreply.github.com>
2020-02-14 13:16:35 -0500

    Fix timer jitter

--- 23-Project17/Project17/View/ContentView.swift
+++ 23-Project17/Project17/View/ContentView.swift
@@ -38,7 +38,8 @@ struct ContentView: View {
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
-    private static let initialTimerValue = 100
+    static let initialTimerValue = 100
+    static let maxTimerDigits = String(Self.initialTimerValue).count
 
     var body: some View {
         ZStack {
@@ -54,7 +55,7 @@ struct ContentView: View {
             VStack {
 
                 // MARK: main UI/time
-                Text("Time: \(timeRemaining)")
+                TimerView(timeRemaining: timeRemaining, style: .variable)
                     .font(.largeTitle)
                     .foregroundColor(.white)
                     .padding(.horizontal, 20)
@@ -284,6 +285,65 @@ struct ContentView: View {
     }
 }
 
+struct TimerView: View {
+    enum Style {
+        case variable, fixed
+    }
+
+    let timeRemaining: Int
+    let style: Style
+    let displayLeadingZeros: Bool = true
+
+    var body: some View {
+        Group {
+            if style == .variable {
+                variableStyleTimer()
+            }
+            else {
+                fixedStyleTimer()
+            }
+        }
+    }
+
+    private func variableStyleTimer() -> some View {
+        VStack(alignment: .leading) {
+            Text("Time: \(timeRemaining)")
+            // avoid jitter
+            Text("Time: \(String(repeating: "0", count: String(timeRemaining).count))")
+                .hidden()
+                .frame(height: 0)
+        }
+    }
+
+    private func fixedStyleTimer() -> some View {
+        HStack(spacing: 0) {
+            Text("Time: ")
+            ForEach(0..<ContentView.maxTimerDigits, id: \.self) { i in
+                VStack(alignment: .center) {
+                    self.timerDigitText(for: i)
+                    // avoid jitter
+                    Text("0").hidden().frame(height: 0)
+                }
+            }
+        }
+    }
+
+    private func timerDigitText(for position: Int) -> Text {
+        let stringTimeRemaining = Array(String(timeRemaining))
+
+        let index = position - (ContentView.maxTimerDigits - stringTimeRemaining.count)
+
+        if index >= 0 {
+            return Text(String(stringTimeRemaining[index]))
+        }
+
+        if displayLeadingZeros {
+            return Text("0").foregroundColor(Color.white.opacity(0.2))
+        }
+
+        return Text("")
+    }
+}
 
 // Challenge 1
 struct RestartView: View {
